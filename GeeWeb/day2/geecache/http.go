66e51f57d88231a507852826c0efa9e8ab7b2ff2
SAM-MBP <ainiruoxia@outlook.com>
2023-04-18 22:51:24 +0800

    feat:http

--- GeeWeb/day2/geecache/http.go
+++ GeeWeb/day2/geecache/http.go
@@ -1,1 +1,57 @@
 package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defalutBasePath = "/_geecache/" // 节点通讯地址的前缀
+
+type HTTPPool struct {
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defalutBasePath,
+	}
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	// 设定一个访问路径 /<basepath>//<groupname>/<key>
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
--- GeeWeb/day2/main.go
+++ GeeWeb/day2/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"geecache"
+	"log"
+	"net/http"
+)
+
+var db = map[string]string{
+	"Tom":  "234",
+	"Jack": "883",
+	"SAM":  "999",
+}
+
+//type server int
+//
+//func (h *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	log.Println(r.URL.Path)
+//	w.Write([]byte("hello world"))
+//}
+
 func main() {
+	//var s server
+	//http.ListenAndServe("0.0.0.0:8082", &s)
+
+	geecache.NewGroup("score", 2<<10, geecache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "0.0.0.0:8082"
+	peers := geecache.NewHTTPPool(addr)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
 
 }
