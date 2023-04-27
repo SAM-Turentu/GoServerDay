41856bb76252af372e124aa74132590041013cb0
SAM-Turentu <SAM-Turentu@outlook.com>
2023-04-27 14:06:36 +0800

    feat:hooks

--- GeeWeb/day3/cmd_test/main.go
+++ GeeWeb/day3/cmd_test/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"geeorm"
+	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
 
 //region main demo
@@ -29,11 +29,30 @@ import (
 
 //endregion
 
+//region Description
+//func main() {
+//	engine, _ := geeorm.NewEngine("sqlite3", "D:\\Common\\Sqlite\\db\\gee.db")
+//	defer engine.Close()
+//	s := engine.NewSession()
+//	result, _ := s.Raw("insert into User values (?, ?)", "hh", 28).Exec()
+//	count, _ := result.RowsAffected()
+//	fmt.Printf("Exec success, %d affected\n", count)
+//}
+//endregion
+
+// region sqlite3 事物
 func main() {
-	engine, _ := geeorm.NewEngine("sqlite3", "D:\\Common\\Sqlite\\db\\gee.db")
-	defer engine.Close()
-	s := engine.NewSession()
-	result, _ := s.Raw("insert into User values (?, ?)", "hh", 28).Exec()
-	count, _ := result.RowsAffected()
-	fmt.Printf("Exec success, %d affected\n", count)
+	db, _ := sql.Open("sqlite3", "D:\\Common\\Sqlite\\db\\gee.db")
+	tx, _ := db.Begin()
+	_, err1 := tx.Exec("insert into User(`Name`) values (?)", "sam")
+	_, err2 := tx.Exec("insert into User(`Age`) values (?)", "Jack")
+	if err1 != nil || err2 != nil {
+		tx.Rollback()
+		log.Println("rollback", err1, err2)
+	} else {
+		tx.Commit()
+		log.Println("commit")
+	}
 }
+
+//endregion
---
+++ GeeWeb/day3/session/hooks_test.go
@@ -0,0 +1,38 @@
+package session
+
+import (
+	"geeorm/log"
+	"testing"
+)
+
+// Account 创建表 字段首字母不能小写
+type Account struct {
+	Id       int `geeorm:"primary key"`
+	Password string
+}
+
+func (account *Account) BeforeInsert(s *Session) error {
+	log.Info("before insert", account)
+	account.Id += 100
+	return nil
+}
+
+func (account *Account) AfterQuery(s *Session) error {
+	log.Info("after query", account)
+	account.Password = "*****"
+	return nil
+}
+
+func TestSession_CallMethod(t *testing.T) {
+	s := NewSession().Model(&Account{})
+	_ = s.DropTable()
+	_ = s.CreateTable()
+	_, _ = s.Insert(&Account{1, "123"}, &Account{2, "asdf"})
+
+	u := &Account{} // 返回第一条记录的account password=‘*****’
+
+	err := s.First(u)
+	if err != nil || u.Id != 101 || u.Password != "*****" {
+		t.Fatal("Failed to call hooks after query, got", u)
+	}
+}
