ad27a114acb544ef92da465cc16601674fa8c262
SAM-MBP <ainiruoxia@outlook.com>
2023-04-30 13:34:02 +0800

    feat: 负载均衡

--- GeeWeb/day4/xclient/discovery.go
+++ GeeWeb/day4/xclient/discovery.go
@@ -1,1 +1,89 @@
 package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// SelectMode 负载均衡模式
+type SelectMode int
+
+const (
+	RandomSelect SelectMode = iota
+	RoundRobinSelect
+)
+
+// Discovery 服务发现
+type Discovery interface {
+	Refresh() error                      // 从注册中心更新服务列表
+	Update(servers []string) error       // 手动更新
+	Get(mode SelectMode) (string, error) // 根据负载均衡策略，选择一个服务实例
+	GetAll() ([]string, error)           // 返回所有服务实例
+}
+
+// MultiServersDiscovery 不需要注册中心，服务列表由手工维护的服务发现的结构体
+type MultiServersDiscovery struct {
+	r       *rand.Rand
+	mu      sync.Mutex
+	servers []string
+	index   int // 记录算法已经轮训到的位置
+}
+
+// MewMultiServerDiscovery 创建一个 MultiServersDiscovery 实例
+func MewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+// Refresh 从注册中心更新服务列表
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+// Update 手动更新
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+// Get 根据负载均衡策略，选择一个服务实例
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// GetAll 返回所有服务实例
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
